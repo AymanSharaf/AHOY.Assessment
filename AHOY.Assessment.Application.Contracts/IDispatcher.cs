970ff63db4d6f6fa94c9a445f9f2034f4236e8b9
AymanAbdelaal <ayman.s.ismaeel@gmail.com>
2022-06-02 01:17:30 +0200

    Added IDispatcher.cs

---
+++ AHOY.Assessment.Application.Contracts/IDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using AHOY.Assessment.Core.Common;
+
+namespace AHOY.Assessment.Application.Contracts
+{
+    public interface IDispatcher
+    {
+        Task DispatchAsync(ICommand command);
+
+        Task<T> DispatchAsync<T>(ICommand<T> command);
+
+        Task<T> DispatchAsync<T>(IQuery<T> query);
+    }
+}
